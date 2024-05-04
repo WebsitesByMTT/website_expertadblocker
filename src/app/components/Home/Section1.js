17696355eb134e99cc07f94afbd529a5691e8611
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-05-04 05:21:25 -0700

    hulu adblocker done

--- src/app/components/Home/Section1.js
+++ src/app/components/Home/Section1.js
@@ -8,8 +8,8 @@ const Section1 = ({ data }) => {
                 <div className={`grid grid-cols-12 w-[90%] mx-auto items-center  rounded-bl-[6.5rem] rounded-br-[6.5rem] min-h-screen `}>
 
                     <div className="col-span-12 pb-32 md:pb-0 order-2 md:order-1 md:col-span-6 space-y-10 text-center md:text-left">
-                        <h1 className={`text-${data.headingColor} text-[4rem] font-[800]  leading-tight capitalize`}>{data?.heading}</h1>
-                        <p className={`text-${data.textColor} text-[1.8rem]  lg:text-2xl  font-[400] md:font-[400] tracking-wide md:w-[80%] w-full`}>{data?.text}</p>
+                        <h1 className={`${data.headingColor} text-[4rem] font-[800]  leading-tight capitalize`}>{data?.heading}</h1>
+                        <p className={`${data.textColor} text-[1.8rem]  lg:text-2xl  font-[400] md:font-[400] tracking-wide md:w-[80%] w-full`}>{data?.text}</p>
                     </div>
                     <div className='col-span-12 order-1 md:order-2 md:col-span-6 flex justify-center'>
                         <BannerAnimation />
