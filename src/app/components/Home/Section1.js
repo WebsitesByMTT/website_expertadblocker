ad4c16f19869f22dec2dca1f97ba571d8c1885c7
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-06-20 21:08:26 -0700

    Reloved Conflicts

--- src/app/components/Home/Section1.js
+++ src/app/components/Home/Section1.js
@@ -9,14 +9,10 @@ const Section1 = ({ data }) => {
 
                     <div className="col-span-12 pb-[6rem] md:pb-[5rem] order-2 md:order-1 md:col-span-6 space-y-10 md::space-y-0 text-center md:text-left">
                         <h1 className={`${data.headingColor} ${data.HeadingFontSize} font-[800]  leading-tight capitalize`}>{data?.heading}</h1>
-<<<<<<< Updated upstream
-                        <p className={`${data.textColor} ${data.textFontSize}  font-[400] md:font-[400] tracking-wide  w-full`}>{data?.text}</p>
-=======
                         <p className={`${data.textColor} ${data.textFontSize}  font-[400] md:font-[400] tracking-wide  w-full`} dangerouslySetInnerHTML={{ __html:data?.text}}></p>
                         <div className='text-[1.8rem]  inline-block rounded-2xl  overflow-hidden'>
                             <a target="_blank" href="https://chromewebstore.google.com/detail/expert-ad-blocker-free-ad/beecichmnnhnljafpeopjcfgedbeiion" className={` ${data.navbar?.mobilebuttonBgColor} ${data.navbar?.mobilebuttonTextColor} shine-button px-8 py-2 border inline-block rounded-2xl`}>ADD TO CHROME IT&apos;S FREE</a>
                         </div>
->>>>>>> Stashed changes
                     </div>
                     <div className='col-span-12  order-1 md:order-2 md:col-span-6 flex justify-center'>
                         <BannerAnimation images={data.animationImages} />
--- src/app/components/Home/Section2.js
+++ src/app/components/Home/Section2.js
@@ -16,16 +16,6 @@ const Section2 = ({ data }) => {
                 <h2 className={`${data.headingColor} text-[4.5rem] font-[700] leading-tight capitalize`}>{data?.heading}</h2>
                 <p className={`md:text-xl ${data.textColor} text-[1.8rem] font-light leading-8 md:leading-6 tracking-wide w-[80%] md:w-[60%]`}>{data.text}</p>
             </div>
-<<<<<<< Updated upstream
-            <div className="relative overflow-hidden mt-20 mx-auto">
-                <div className="absolute inset-0 z-20 before:absolute before:left-0 before:top-0 before:w-1/4 before:h-full before:bg-gradient-to-r before:filter before:blur-3 after:absolute after:right-0 after:top-0 after:w-1/4 after:h-full after:bg-gradient-to-l after:filter after:blur-3"></div>
-                <div className="slider">
-                    <div className="slide-track">
-                        {duplicatedItems.map((icon, index) => (
-                            <div key={index} className="slide">
-                                <Image src={icon.src} height={180} width={100} alt={icon.alt} className='h-auto w-[90%]'/>
-                            </div>
-=======
             <div className=" mt-10 mb-16 mx-auto">
                 <div className=" inset-0 z-20  before:left-0 before:top-0 before:w-1/4 before:h-full before:bg-gradient-to-r before:filter before:blur-3 after:absolute after:right-0 after:top-0 after:w-1/4 after:h-full after:bg-gradient-to-l after:filter after:blur-3"></div>
                 <div>
@@ -34,7 +24,6 @@ const Section2 = ({ data }) => {
                             <Link href={icon.link} key={index} className="cursor-pointer shadow-2xl bg-white rounded-lg py-3 px-5 border">
                                 <Image src={icon.src} height={180} width={100} alt={icon.alt} quality={100} className='w-[250px]'/>
                             </Link>
->>>>>>> Stashed changes
                         ))}
                     </div>
                 </div>
